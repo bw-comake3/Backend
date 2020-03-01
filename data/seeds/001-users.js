b3de912c60329f26cb912e0bf32fbddc4395fab8
nicbongo <nicbongo@protonmail.com>
2020-03-01 12:58:57 -0400

    seed data hasehed

--- data/seeds/001-users.js
+++ data/seeds/001-users.js
@@ -1,17 +1,18 @@
+const bcrypt = require('bcryptjs');
 
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
+
   return knex('users').truncate()
     .then(function () {
 
-      const userSeed = 
-        [
-          { username: 'Nic', password: 'Test123', user_type: 'user' },
-          { username: 'Ammon', password: 'Test123', user_type: 'user' },
-          { username: 'Thomas', password: 'Test123', user_type: 'user' }
+      const userSeed = [
+          { username: 'Nic', password: bcrypt.hashSync('Test123!', 12), user_type: 'user' },
+          { username: 'Ammon', password: bcrypt.hashSync('Test123!', 12), user_type: 'user' },
+          { username: 'Thomas', password: bcrypt.hashSync('Test123!', 12), user_type: 'user' },
+          { username: 'Michael', password: bcrypt.hashSync('Test123!', 12), user_type: 'user' },
+          { username: 'Devoughn', password: bcrypt.hashSync('Test123!', 12), user_type: 'user' }
         ]
       
-      // Inserts seed entries
       return knex('users').insert(userSeed);
     });
 };
