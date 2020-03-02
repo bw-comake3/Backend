eaba5ea57b0cb83ddd920a0c27c3033b4119a511
Ammon Matthews <29493963+matthews7@users.noreply.github.com>
2020-03-02 14:07:02 -0700

    its working

--- data/seeds/002-issues.js
+++ data/seeds/002-issues.js
@@ -3,12 +3,12 @@ exports.seed = function(knex) {
   // Deletes ALL existing entries
   return knex('issues').del()
     .then(function () {
-      const issuesSeed = [
-        { issue: "issue1", description: "bla bla bla1", vote: 1, zip: "123456", city: "Miami1", user_id: 1 },
-        { issue: "issue2", description: "bla bla bla2", vote: 2, zip: "123456", city: "Miami2", user_id: 2 },
-        { issue: "issue3", description: "bla bla bla3", vote: 3, zip: "123456", city: "Miami3", user_id: 3 },
-        { issue: "issue4", description: "bla bla bla4", vote: 4, zip: "123456", city: "Miami4", user_id: 4 }      
-      ]
+      // const issuesSeed = [
+      //   { issue: "issue1", description: "bla bla bla1", vote: 1, zip: "123456", city: "Miami1", user_id: 1 },
+      //   { issue: "issue2", description: "bla bla bla2", vote: 2, zip: "123456", city: "Miami2", user_id: 2 },
+      //   { issue: "issue3", description: "bla bla bla3", vote: 3, zip: "123456", city: "Miami3", user_id: 3 },
+      //   { issue: "issue4", description: "bla bla bla4", vote: 4, zip: "123456", city: "Miami4", user_id: 4 }      
+      // ]
       return knex('issues').insert([
         { issue: "issue1", description: "bla bla bla1", vote: 1, zip: "123456", city: "Miami1", user_id: 1 },
         { issue: "issue2", description: "bla bla bla2", vote: 2, zip: "123456", city: "Miami2", user_id: 2 },
