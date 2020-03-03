9d28c06b817ce2799b69dfac7d87ef52fb29af63
nicbongo <nicbongo@protonmail.com>
2020-03-03 13:36:48 -0400

    MVP
    Co-authored-by: Ammon Matthews <ammonmatthews@gmail.com>

---
+++ api/server.spec.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+
+
+
+describe('Server operating in correct environment', () => {
+  it('Server working in test environoment', () => {
+    expect(process.env.DB_ENV).toBe('testing');
+  })
+})
+
+
+    
+    
