6430d99556a1473704f6e7909eba2ce9e5d50e86
Ammon Matthews <29493963+matthews7@users.noreply.github.com>
2020-03-03 12:58:07 -0700

    seed update

--- data/seeds/001-users.js
+++ data/seeds/001-users.js
@@ -4,15 +4,6 @@ exports.seed = function(knex) {
 
   return knex('users').del()
     .then(function () {
-
-      // const userSeed = [
-      //     { username: 'Nic', password: bcrypt.hashSync('Test123!', 12), user_type: 'user' },
-      //     { username: 'Ammon', password: bcrypt.hashSync('Test123!', 12), user_type: 'user' },
-      //     { username: 'Thomas', password: bcrypt.hashSync('Test123!', 12), user_type: 'user' },
-      //     { username: 'Michael', password: bcrypt.hashSync('Test123!', 12), user_type: 'user' },
-      //     { username: 'Devoughn', password: bcrypt.hashSync('Test123!', 12), user_type: 'user' }
-      //   ]
-      
       return knex('users').insert([
          { username: 'Nic', password: bcrypt.hashSync('Test123!', 12), user_type: 'user' },
           { username: 'Ammon', password: bcrypt.hashSync('Test123!', 12), user_type: 'user' },
