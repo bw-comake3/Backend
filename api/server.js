a6561a4761fbec3cd3912efe6656154196fa9aad
Ammon Matthews <29493963+matthews7@users.noreply.github.com>
2020-03-06 11:01:39 -0700

    fixing bugs

--- api/server.js
+++ api/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express');
 
 const middlewareConfig = require('./middleware-config.js');
--- knexfile.js
+++ knexfile.js
@@ -4,13 +4,17 @@ module.exports = {
     client: 'pg',
     useNullAsDefault: true,
     // connection: "postgres://localhost:5300/comake",
-    connection: {
-      host: "127.0.0.1",
-      port: "5300",
-      user: "postgres",
-      password: process.env.DB_PASSWORD,
-      database: "comake"
-    },    
+    connection: { // use this for for sqlite3 local dev
+      filename: './data/comake.db3'
+
+    },
+    // connection: {
+    //   host: "127.0.0.1",
+    //   port: "5300",
+    //   user: "postgres",
+    //   password: process.env.DB_PASSWORD,
+    //   database: "comake"
+    // },    
     migrations: {
       directory: './data/migrations',
       tableName: 'knex_migrations'
